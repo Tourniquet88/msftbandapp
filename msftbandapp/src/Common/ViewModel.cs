319dde0b582050faa60e6a9f5e679dde225971ed
James Walker <jh_walker@outlook.com>
2019-06-04 20:32:26 +0100

    Implement base ViewModel

--- msftbandapp/src/Common/ViewModel.cs
+++ msftbandapp/src/Common/ViewModel.cs
@@ -5,15 +5,33 @@ using System.Runtime.CompilerServices;
 
 namespace MSFTBandApp.Common {
 
-/// <summary>View model base</summary>
+/// <summary>
+/// View model base
+/// 
+/// A basic MVVM implementation.
+/// 
+/// Extend and add properties using getter/setter pattern to 
+/// 	to invoke `PropertyAccess`/`PropertyChange` respectively, 
+/// 	which handle accessing property data in a corresponding private 
+/// 	field (which must be named as the property name with a `_` prefix) 
+/// 	and invoking `OnPropertyChanged` to update XAML bindings.
+/// 
+/// You can also use `OnPropertyChanged` manually to update a property 
+/// 	in the view (`View`, which is passed at construction). 
+/// </summary>
 public class ViewModel : INotifyPropertyChanged {
 
-	public object host;
+	/// <summary>View instance the model is attached to</summary>
+	public object View;
 
+	/// <summary>Property changed event handler for changes</summary>
 	public event PropertyChangedEventHandler PropertyChanged;
 
-	public ViewModel(object host) {
-		this.host = host;
+
+	/// <summary>Construct with a given view instance.</summary>
+	/// <param name="view">View instance the model is attached to</param>
+	public ViewModel(object view) {
+		this.View = view;
 	}
 
 
@@ -41,17 +59,30 @@ public class ViewModel : INotifyPropertyChanged {
 		T value, [CallerMemberName] string prop=null) {
 
 		this.GetField($"_{prop}").SetValue(this, value);
-		this.OnPropertyChanged();
+		this.OnPropertyChanged(prop);
 	}
 
 
-	public FieldInfo GetField(string prop) {
-		return this.GetType().GetRuntimeField(prop);
+	/// <summary>Get the `FieldInfo` for a given field name.</summary>
+	/// <param name="prop">field</param>
+	/// <returns>FieldInfo</returns>
+	public FieldInfo GetField(string field) {
+		return this.GetType().GetRuntimeField(field);
 	}
 
 
-	protected virtual void OnPropertyChanged([CallerMemberName] string name="") {
-		this.PropertyChanged?.Invoke(this.host, new PropertyChangedEventArgs(name));
+	/// <summary>
+	/// `INotifyPropertyChanged` implementation.
+	/// 
+	/// Notifies `this.View` – the view the model is attached to.
+	/// </summary>
+	/// <param name="name">The property which changed</param>
+	protected virtual void OnPropertyChanged(
+		[CallerMemberName] string name="") {
+
+		this.PropertyChanged?.Invoke(
+			this.View, new PropertyChangedEventArgs(name)
+		);
 	}
 
 }
