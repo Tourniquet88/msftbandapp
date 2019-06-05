fe507473fbc45ce09225ef332a8c016404d52d55
James Walker <jh_walker@outlook.com>
2019-06-05 16:25:47 +0100

    LaunchPage

---
+++ msftbandapp/src/LaunchPage/LaunchPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MSFTBandApp.LaunchPage {
+
+/// <summary>Launch page</summary>
+public partial class LaunchPage : ContentPage {
+	
+	/// <summary>Construct the page</summary>
+	/// <returns>public</returns>
+	public LaunchPage() {
+		this.InitializeComponent();
+	}
+
+}
+
+}
